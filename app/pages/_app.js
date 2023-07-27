62a5402fc0e8a3f56354f800f5de8b3b47b5e3ab
shayan1234554321 <kshayan091@gmail.com>
2023-07-27 16:10:23 +0500

    updated styling

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -5,7 +5,7 @@ import logo from '../assets/logo.svg'
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 border-b border-b-[#e1e7f1]' >
+      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-2 border-b border-b-[#e1e7f1]' >
         <Link href="/" >
           <img src={logo.src} alt="logo" className='w-28 object-contain' />
         </Link>
